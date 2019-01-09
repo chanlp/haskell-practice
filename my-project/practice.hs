a48e40ac96ef9c86a019a106a1bdd7bcebbc8169
Chandler Petersen <chandlerpetersen@outlook.com>
2019-01-08 17:48:43 -0800

    Built up complex functions by combining simpler ones

--- my-project/practice.hs
+++ my-project/practice.hs
@@ -7,6 +7,29 @@
 
 -- This is a pretty interesting concept --
 -- Note: These are singly linked-lists, not arrays
-c = [2, 3, 4] == 2 : 3 : 4 : []
-main = print (c)
+-- c = [2, 3, 4] == 2 : 3 : 4 : []
+-- main = print (c)
 
+trial :: Integer -> Integer
+trial n
+	| n `mod` 2 == 0 = n `div` 2 -- results in remainder 0 when div by 2
+	| otherwise		 = 3 * n + 1 -- otherwise == true
+
+trialSeq :: Integer -> [Integer]
+trialSeq 1 = [1]
+trialSeq n = n : trialSeq (trial n)
+
+-- Compute the length of a list of integers
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (x:xs) = 1 + intListLength xs
+
+-- show
+-- The number of trial steps needed to reach 1 from a given number
+trialLen :: Integer -> Integer
+trialLen n = intListLength (trialSeq n) - 1
+
+-- Each element of the sequence is only generated as needed
+-- The sequence generation and list length calc are interleaved
+-- Complexity: O(1)
+main = print (trialLen 5)
