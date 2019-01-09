8d5e35e71543650f13935a73e95262a2e602c1ee
Chandler Petersen <chandlerpetersen@outlook.com>
2019-01-08 17:35:49 -0800

    more practice

--- my-project/practice.hs
+++ my-project/practice.hs
@@ -1,6 +1,12 @@
 #!/usr/bin/env stack
 -- stack --install-ghc runghc
 
-module Main where
-main :: IO ()
-main = putStrLn "This is a test"
+--module Main where
+--main :: IO ()
+--main = putStrLn "This is a test"
+
+-- This is a pretty interesting concept --
+-- Note: These are singly linked-lists, not arrays
+c = [2, 3, 4] == 2 : 3 : 4 : []
+main = print (c)
+
